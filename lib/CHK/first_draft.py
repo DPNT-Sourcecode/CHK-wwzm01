1e9526734553c8a93358ee95a9f8bf2c5cf3d091
sbx_user1051 <sbx_user1051@169.254.173.5>
2022-07-12 09:40:39 +0000

    Tue Jul 12 09:40:39 UTC 2022

--- lib/CHK/first_draft.py
+++ lib/CHK/first_draft.py
@@ -25,17 +25,28 @@ def find_total_values(SKUs):
     return {"A":a, "B":b, "C":c, "D":d}
     
 def work_out_cost(product_amounts):
-    total_cost = int(product_amounts["A"]/3) * 130
-    total_cost += product_amounts["A"] % 3 * 50
-    total_cost += int(product_amounts["B"]/2) * 45
-    total_cost += product_amounts["A"] % 2 * 30
-    total_cost += product_amounts["C"] 
-    total_cost += product_amounts["D"]
+    total_cost = 0
+    if product_amounts["A"] >= 3:
+        total_cost += int(product_amounts["A"]/3) * 130
+        total_cost += product_amounts["A"] % 3 * 50
+    else:
+        total_cost += product_amounts["A"] * 50
+
+    if product_amounts["B"] >= 2:
+        total_cost += int(product_amounts["B"]/2) * 45
+        total_cost += product_amounts["B"] % 2 * 30
+    else:
+        total_cost += product_amounts["B"] * 30
+
+    total_cost += product_amounts["C"] * 20
+    total_cost += product_amounts["D"] * 15
     return total_cost 
     
 
+print(main("ABCD")) #should output 115
 print(main("AAABCD")) #should output 195
 print(main("AAAABCD")) #should output 245
 print(main("AAABBCD")) #should output 210
 print(main("AAAEBBCD")) #should output -1
+
 
