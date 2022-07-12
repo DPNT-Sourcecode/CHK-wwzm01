a19517bc68620b6844e402874cae027df3ecc612
sbx_user1051 <sbx_user1051@169.254.173.5>
2022-07-12 09:25:39 +0000

    Tue Jul 12 09:25:39 UTC 2022

--- lib/CHK/first_draft.py
+++ lib/CHK/first_draft.py
@@ -1,2 +1,18 @@
 def main(SKUs):
-    
+    a = 0
+    b = 0
+    c = 0
+    d = 0 #create a running total of the total amount of each item in basket
+    for s in SKUs:
+        if s == "A":
+            a += 1
+        elif s == "B":
+            b += 1
+        elif s == "C":
+            c += 1
+        elif s == "D":
+            d += 1   #here we add 1 on to the running total for each item in the basket
+        else:
+            return -1   #if an incorrect input has been entered return -1
+
+
