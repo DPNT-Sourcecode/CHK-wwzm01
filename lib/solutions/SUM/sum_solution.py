d98636f1629176ab53232b4a4f244141862067af
sbx_user1051 <sbx_user1051@169.254.127.221>
2022-07-12 08:59:07 +0000

    Tue Jul 12 08:59:07 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    if x > 0 and x < 100 and y > 0 and y < 100: #make sure integers fall within the parameters
+        result = x + y #sum the integers
+        return result #return the result
+    else:
+        return "One or both integers did not fall within the parameters" #return error message
+
